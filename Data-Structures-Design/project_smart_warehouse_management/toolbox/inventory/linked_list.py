36f3b6837f1c8a3d210ad5fe45271c4139d13c11
Xinrui Zhang <2805123911@qq.com>
2024-07-01 15:34:16 +0800

    date 2024.6.26 v1.1
    · In version 1.1, we have added the loader.py implementation, which enables loading data from a CSV file and adding it to a LinkedList.
    · Additionally, we have made minor optimizations to the overall code structure and standardized the code format for setting system paths when importing modules.
    · We have also included test_loader.py in the unittest framework to verify the executability of the loader.py file.

--- Data-Structures-Design/project_smart_warehouse_management/toolbox/inventory/linked_list.py
+++ Data-Structures-Design/project_smart_warehouse_management/toolbox/inventory/linked_list.py
@@ -4,11 +4,10 @@ import os
 # 获取当前脚本文件的目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# 将当前脚本文件目录的上级目录添加到系统路径
-toolbox_dir = os.path.dirname(current_dir)
-sys.path.append(toolbox_dir)
+# 将上级目录（项目根目录）添加到系统路径中
+project_dir = os.path.dirname(current_dir)
+sys.path.append(project_dir)
 
-# 导入自定义模块
 from inventory.item import Item
 
 class Node:
--- Data-Structures-Design/tests/test_inventory/test_linked_list.py
+++ Data-Structures-Design/tests/test_inventory/test_linked_list.py
@@ -5,9 +5,9 @@ import os
 # 获取当前脚本文件的目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# 将当前脚本文件目录的上级目录添加到系统路径
-toolbox_dir = os.path.dirname(os.path.dirname(current_dir))
-sys.path.append(toolbox_dir)
+# 将上级目录（项目根目录）添加到系统路径中
+project_dir = os.path.dirname(os.path.dirname(current_dir))
+sys.path.append(project_dir)
 
 # 导入需要测试的类
 from project_smart_warehouse_management.toolbox.inventory.item import Item, Category, Priority
