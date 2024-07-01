8090a98c5b9631393f69b6924ad8d3627b5fa495
Xinrui Zhang <2805123911@qq.com>
2024-07-01 15:51:42 +0800

    date 2024.6.25 v1.0

--- Smart_Warehouse_Management/tests/test_inventory/test_item.py
+++ Smart_Warehouse_Management/tests/test_inventory/test_item.py
@@ -2,26 +2,27 @@ import unittest
 import sys
 import os
 
-# Get the current directory of the script file
-current_dir = os.path.dirname(os.path.abspath(__file__))
+# 获取当前文件所在的目录
+current_dir = os.path.dirname(__file__)
 
-# Add the parent directory (project root directory) to the system path
-project_dir = os.path.dirname(os.path.dirname(current_dir))
-sys.path.append(project_dir)
+# 获取项目根目录的路径
+project_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
 
-# Import the class to be tested
-from project_smart_warehouse_management.toolbox.inventory.item import Item, Category, Priority
+# 添加项目根目录到sys.path
+sys.path.insert(0, project_dir)
+
+# 导入需要测试的类
+from project_smart_warehouse_management.inventory.item import Item, Category, Priority
 
 class TestItem(unittest.TestCase):
 
     def test_item_creation(self):
-        # Test the creation of an Item object
         item = Item(1, "Laptop", Category.ELECTRONICS, 10, Priority.HIGH)
-        self.assertEqual(item.item_ID, 1)                     
-        self.assertEqual(item.name, "Laptop")                 
-        self.assertEqual(item.category, Category.ELECTRONICS) 
-        self.assertEqual(item.quantity, 10)                   
-        self.assertEqual(item.priority_level, Priority.HIGH)  
+        self.assertEqual(item.item_ID, 1)
+        self.assertEqual(item.name, "Laptop")
+        self.assertEqual(item.category, Category.ELECTRONICS)
+        self.assertEqual(item.quantity, 10)
+        self.assertEqual(item.priority_level, Priority.HIGH)
 
 if __name__ == '__main__':
     unittest.main()
